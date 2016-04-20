1d81967e49175c1d6e32ae869fd6b89603f26afc
Amit Murthy <amit.murthy@gmail.com>
2016-04-20 15:40:10 +0530

    configure pmap via keyword args

--- base/asyncmap.jl
+++ base/asyncmap.jl
@@ -4,7 +4,8 @@
 """
     AsyncCollector(f, results, c...; ntasks=100) -> iterator
 
-Apply f to each element of c using at most 100 asynchronous tasks.
+Apply f to each element of c using at most max(100, nworkers()) asynchronous
+tasks.
 For multiple collection arguments, apply f elementwise.
 Output is collected into "results".
 
@@ -22,17 +23,17 @@ end
 
 function AsyncCollector(f, results, c...; ntasks=0)
     if ntasks == 0
-        ntasks = 100
+        ntasks = max(nworkers(), 100)
     end
     AsyncCollector(f, results, enumerate(zip(c...)), ntasks)
 end
-
 
 type AsyncCollectorState
     enum_state
     active_count::Int
     task_done::Condition
     done::Bool
+    in_error::Bool
 end
 
 
@@ -49,11 +50,20 @@ wait(state::AsyncCollectorState) = wait(state.task_done)
 # Open a @sync block and initialise iterator state.
 function start(itr::AsyncCollector)
     sync_begin()
-    AsyncCollectorState(start(itr.enumerator),  0, Condition(), false)
+    AsyncCollectorState(start(itr.enumerator),  0, Condition(), false, false)
 end
 
 # Close @sync block when iterator is done.
 function done(itr::AsyncCollector, state::AsyncCollectorState)
+    if state.in_error
+        sync_end()
+
+        # state.in_error is only being set in the @async block (and an error thrown),
+        # which in turn should have been caught and thrown by the sync_end() call above.
+        # Control should not come here.
+        @assert false "Error should have been captured and thrown previously."
+    end
+
     if !state.done && done(itr.enumerator, state.enum_state)
         state.done = true
         sync_end()
@@ -62,24 +72,34 @@ function done(itr::AsyncCollector, state::AsyncCollectorState)
 end
 
 function next(itr::AsyncCollector, state::AsyncCollectorState)
-    # Wait if the maximum number of concurrent tasks are already running
+    # Wait if the maximum number of concurrent tasks are already running.
     while isbusy(itr, state)
         wait(state)
     end
 
-    # Get index and mapped function arguments from enumeration iterator
+    # Get index and mapped function arguments from enumeration iterator.
     (i, args), state.enum_state = next(itr.enumerator, state.enum_state)
 
-    # Execute function call and save result asynchronously
+    # Execute function call and save result asynchronously.
     @async begin
-        itr.results[i] = itr.f(args...)
-        state.active_count -= 1
+        try
+            itr.results[i] = itr.f(args...)
+        catch e
+            state.in_error = true
+            notify(state.task_done, e; error=true)
+
+            # The "notify" above raises an exception if "next" is waiting for tasks to finish.
+            # If the calling task is waiting on sync_end(), the rethrow() below will be captured
+            # by it.
+            rethrow(e)
+        finally
+            state.active_count -= 1
+        end
         notify(state.task_done, nothing)
     end
 
-    # Count number of concurrent tasks
+    # Count number of concurrent tasks.
     state.active_count += 1
-
     return (nothing, state)
 end
 
@@ -115,7 +135,7 @@ function done(itr::AsyncGenerator, state::AsyncGeneratorState)
     done(itr.collector, state.async_state) && isempty(itr.collector.results)
 end
 
-# Pump the source async collector if it is not already busy
+# Pump the source async collector if it is not already busy.
 function pump_source(itr::AsyncGenerator, state::AsyncGeneratorState)
     if !isbusy(itr.collector, state.async_state) &&
        !done(itr.collector, state.async_state)
@@ -132,7 +152,7 @@ function next(itr::AsyncGenerator, state::AsyncGeneratorState)
     results = itr.collector.results
     while !haskey(results, state.i)
 
-        # Wait for results to become available
+        # Wait for results to become available.
         if !pump_source(itr,state) && !haskey(results, state.i)
             wait(state.async_state)
         end
--- base/pmap.jl
+++ base/pmap.jl
@@ -1,5 +1,9 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
+type BatchProcessingError <: ErrorException
+    data
+    ex
+end
 
 """
     pgenerate([::WorkerPool], f, c...) -> iterator
@@ -15,38 +19,141 @@ Note that `f` must be made available to all worker processes; see
 and Loading Packages <man-parallel-computing-code-availability>`)
 for details.
 """
-function pgenerate(p::WorkerPool, f, c)
-    if length(p) == 0
-        return AsyncGenerator(f, c)
+function pgenerate(p::WorkerPool, f, c; distributed=true, batch_size=1, on_error=nothing, retry=nothing)
+    f_orig = f
+
+    # Don't do remote calls if there are no workers.
+    if (length(p) == 0) || (length(p) == 1 && fetch(p.channel) == myid())
+        distributed = false
     end
-    batches = batchsplit(c, min_batch_count = length(p) * 3)
-    return flatten(AsyncGenerator(remote(p, b -> asyncmap(f, b)), batches))
+
+    # Don't do batching if not doing remote calls.
+    if !distributed
+        batch_size = 1
+    end
+
+    # If not batching, do simple remote call.
+    if batch_size == 1
+        if distributed
+            f = remote(p, f)
+        end
+
+        f = wrap_retry(f, retry)
+        f = handle_on_error(f, on_error)
+        return AsyncGenerator(f, c)
+    else
+        batches = batchsplit(c, min_batch_count = length(p) * 3,
+                                max_batch_size = batch_size)
+
+        # We need to ensure that if on_error is set, it is called for each element
+        # in error, and that we return as many elements as the original list.
+        # retry, if set, has to be called element wise and we will do a best-effort
+        # to ensure that we do not call mapped function on the same element twice.
+        # This guarantee is not possible in case of worker death / network errors, wherein
+        # we will retry the entire batch on a new worker.
+        f = handle_on_error(f, (x,e)->BatchProcessingError(x,e); capture_data=true)
+
+        f = wrap_batch(f, p, on_error)
+
+        results = flatten(AsyncGenerator(f, batches))
+
+        # Handle all the ones in error in another pmap, with batch size set to 1
+        if (on_error != nothing) || (retry != nothing)
+            reprocess = []
+            for (idx, v) in enumerate(results)
+                if isa(v, BatchProcessingError)
+                    push!(reprocess, (i,v))
+                end
+            end
+
+            if length(reprocess) > 0
+                errors = [x->x[2] for x in reprocess]
+                data_in_error = [x.data for x in errors]
+                processed_errors = pmap(p, f_orig, data_in_error; on_error=on_error, retry=retry)
+                for (idx, v) in enumerate(processed_errors)
+                    results[reprocess[idx][1]] = v
+                end
+            end
+        end
+
+        return results
+    end
 end
 
-pgenerate(p::WorkerPool, f, c1, c...) = pgenerate(p, a->f(a...), zip(c1, c...))
+pgenerate(p::WorkerPool, f, c1, c...; kwargs...) = pgenerate(p, a->f(a...), zip(c1, c...); kwargs...)
 
-pgenerate(f, c) = pgenerate(default_worker_pool(), f, c...)
-pgenerate(f, c1, c...) = pgenerate(a->f(a...), zip(c1, c...))
+pgenerate(f, c; kwargs...) = pgenerate(default_worker_pool(), f, c...; kwargs...)
+pgenerate(f, c1, c...; kwargs...) = pgenerate(a->f(a...), zip(c1, c...); kwargs...)
+
+function wrap_on_error(f, on_error; capture_data=false)
+    if on_error != nothing
+        return x -> begin
+            try
+                f(x)
+            catch e
+                if capture_data
+                    on_error(x, e)
+                else
+                    on_error(e)
+                end
+            end
+        end
+    else
+        return f
+    end
+end
+
+wrap_retry(f, retryf) = (retryf != nothing ? retryf(f) : f)
+
+function wrap_batch(f, p, on_error)
+    f = asyncmap_batch(f)
+    return batch -> begin
+        try
+            remotecall_fetch(f, p, batch)
+        catch e
+            if on_error != nothing
+                return Any[BatchProcessingError(batch[i], e) for i in 1:length(batch)]
+            else
+                rethrow(e)
+            end
+        end
+    end
+end
+
+asyncmap_batch(f) = batch -> asyncmap(f, batch)
 
 
 """
-    pmap([::WorkerPool], f, c...) -> collection
+    pmap([::WorkerPool], f, c...; distributed=true, batch_size=1, on_error=nothing) -> collection
 
 Transform collection `c` by applying `f` to each element using available
 workers and tasks.
 
 For multiple collection arguments, apply f elementwise.
 
-Note that `err_retry=true` and `err_stop=false` are deprecated,
-use `pmap(retry(f), c)` or `pmap(@catch(f), c)` instead
-(or to retry on a different worker, use `asyncmap(retry(remote(f)), c)`).
-
 Note that `f` must be made available to all worker processes; see
 [Code Availability and Loading Packages](:ref:`Code Availability
 and Loading Packages <man-parallel-computing-code-availability>`)
 for details.
+
+If a worker pool is not specified, all available workers, i.e., the default worker pool
+is used.
+
+By default, `pmap` distributes the computation over all specified workers. To use only the
+local process and distribute over tasks, specifiy `distributed=false`
+
+`pmap` can also use a mix of processes and tasks via the `batch_size` argument. For batch sizes
+greater than 1, the collection is split into multiple batches, which are distributed across
+workers. Each such batch is processed in parallel via tasks in each worker. The specified
+`batch_size` is an upper limit, the actual size of batches may be smaller and is calculated
+depending on the number of workers available and length of the collection.
+
+Any error stops pmap from processing the remainder of the collection. To override this behavior
+you can specify an error handling function via argument `on_error` which takes in a single argument, i.e.,
+the exception. The function can stop the processing by rethrowing the error, or, to continue, return any value
+which is then returned inline with the results to the caller.
 """
-pmap(p::WorkerPool, f, c...) = collect(pgenerate(p, f, c...))
+pmap(p::WorkerPool, f, c...; kwargs...) = collect(pgenerate(p, f, c...; kwargs...))
 
 
 """
@@ -72,7 +179,7 @@ function batchsplit(c; min_batch_count=1, max_batch_size=100)
     # If there are not enough batches, use a smaller batch size
     if length(head) < min_batch_count
         batch_size = max(1, div(sum(length, head), min_batch_count))
-        return partition(flatten(head), batch_size)
+        return partition(collect(flatten(head)), batch_size)
     end
 
     return flatten((head, tail))
